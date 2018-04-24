8db4a36604e16ac1761ea058a76e308f7722c64a
elhoncho <elhoncho>
2018-04-24 03:00:46 -0500

    Added c files

---
+++ main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: Mar 3, 2018
+ *      Author: owner
+ */
+
+#ifndef AT86RF212B_INC_MAIN_H_
+#define AT86RF212B_INC_MAIN_H_
+#include<stdint.h>
+uint8_t AT86RF212B_Mode;
+
+#endif /* AT86RF212B_INC_MAIN_H_ */
