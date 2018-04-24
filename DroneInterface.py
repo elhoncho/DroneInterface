3867696d67f50186df1e06f232a1d21b0a6531f2
elhoncho <elhoncho>
2018-04-24 04:13:44 -0500

    Updates

--- DroneInterface.py
+++ DroneInterface.py
@@ -1,11 +1,14 @@
 import time
+import sys
 from dronekit import connect, VehicleMode
 
 # Connect to the Vehicle.
 vehicle = connect("/dev/serial0", baud=57600, wait_ready=True)
 
 while True:
-    print " GPS: %s" % vehicle.location.global_frame
+    output = " GPS: %s" % vehicle.location.global_frame
+    sys.stdout.write(output)
+    print("\r\n")
     time.sleep(5)
 # Close vehicle object before exiting script
 vehicle.close()
