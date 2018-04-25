a934686266025009c0ee1537e56554168c1a27c5
elhoncho <elhoncho>
2018-04-25 11:43:36 -0500

    Refactored Code

--- DroneInterface.py
+++ DroneInterface.py
@@ -1,14 +1,29 @@
+import subprocess
 import time
-import sys
 from dronekit import connect, VehicleMode
 
-# Connect to the Vehicle.
 vehicle = connect("/dev/serial0", baud=57600, wait_ready=True)
+gpsData = "%s\n" % vehicle.location.global_frame
 
-while True:
-    output = " GPS: %s" % vehicle.location.global_frame
-    sys.stdout.write(output)
-    print("\r\n")
-    time.sleep(5)
-# Close vehicle object before exiting script
+p = subprocess.Popen(['./RaspberryPi'],
+                     stdin=subprocess.PIPE)
+p.stdin.write(gpsData)
+
+print("Basic pre-arm checks")
+while not vehicle.is_armable:
+    print(" Waiting for vehicle to initialise...")
+    time.sleep(1)
+
+print("Arming Motors")
+vehicle.mode = VehicleMode("GUIDED")
+vehicle.armed = True
+
+while not vehicle.armed:
+    print("Waiting for arming...")
+    time.sleep(1)
+
+print("Taking off!")
+vehicle.simple_takeoff(10) # Take off to target altitude
+
 vehicle.close()
+p.kill()
