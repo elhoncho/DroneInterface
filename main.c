eceea79b40b0eda40a0c8426a6ea66cf02814a64
elhoncho <elhoncho>
2018-04-25 09:36:57 -0500

    Updates

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #include <stdint.h>
 #include <termios.h>
 #include "wiringPi.h"
-#include "MainController.h"
+#include "AT86RF212B/Inc/MainController.h"
+#include "AT86RF212B/Inc/AT86RF212B.h"
 
 #define SPI_NSS_PIN 3
 #define CLKM_PIN 0
@@ -16,27 +17,7 @@
 #define DIG2_PIN 2
 
 int main(){
-//	struct termios term, term_orig;
-
 	AT86RF212B_Open();
-
-//	//Set the STDIN to canonical mode so that one char can be read with read without needing a new line
-//	if(tcgetattr(0, &term_orig)) {
-//		printf("tcgetattr failed\n");
-//		return -1;
-//	}
-//
-//	term = term_orig;
-//
-//	term.c_lflag &= ~ICANON;
-//	term.c_lflag &= ~ECHO;
-//	term.c_cc[VMIN] = 0;
-//	term.c_cc[VTIME] = 0;
-//
-//	if (tcsetattr(0, TCSANOW, &term)) {
-//		printf("tcsetattr failed\n");
-//		return -1;
-//	}
 
 	int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
 	fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
