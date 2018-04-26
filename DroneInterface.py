3071885898a02de56d0b743b1782d2cf52325630
elhoncho <elhoncho>
2018-04-26 11:30:04 +0000

    Updated python scrip

--- DroneInterface.py
+++ DroneInterface.py
@@ -1,13 +1,43 @@
-import subprocess
 import time
+import sys
 from dronekit import connect, VehicleMode
+from subprocess import PIPE, Popen
+from threading  import Thread
+
+newCmd = 0
+
+try:
+    from Queue import Queue, Empty
+except ImportError:
+    from queue import Queue, Empty  # python 3.x
+
+ON_POSIX = 'posix' in sys.builtin_module_names
+
+def enqueue_output(out, queue):
+    #for line in iter(lambda: out.read(1), b''):
+    for line in iter(out.readline, b''):
+        queue.put(line)
+    out.close()
+
+def ReadRadioInput():
+    while True:
+        try:
+            line = q.get_nowait() # or q.get(timeout=.1)
+        except Empty:
+            break
+        else:
+            print("Got: "+line)
+            newCmd = 1
+            command = line
+
+p = Popen(['./RaspberryPiDrone'], stdin=PIPE, stdout=PIPE, bufsize=1, close_fds=ON_POSIX)
+q = Queue()
+t = Thread(target=enqueue_output, args=(p.stdout, q))
+t.daemon = True # thread dies with the program
+t.start()
 
 vehicle = connect("/dev/serial0", baud=57600, wait_ready=True)
 gpsData = "%s\n" % vehicle.location.global_frame
-
-p = subprocess.Popen(['../RaspberryPiDrone'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-p.stdin.write(gpsData)
-p.stdout.write("Hello from the drone :)\r\n");
 
 print("Basic pre-arm checks")
 while not vehicle.is_armable:
@@ -22,8 +52,15 @@ while not vehicle.armed:
     print("Waiting for arming...")
     time.sleep(1)
 
-print("Taking off!")
-vehicle.simple_takeoff(10) # Take off to target altitude
+#print("Taking off!")
+#vehicle.simple_takeoff(10) # Take off to target altitude
+
+while True:
+    ReadRadioInput()
+    time.sleep(1)
+    if newCmd == 1:
+        newCmd = 0
+        exec(command)
 
 vehicle.close()
 p.kill()
